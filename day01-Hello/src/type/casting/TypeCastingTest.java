472a11391b914dce706b82ae649f09b7a34bfdfa
채한나 <hannacne@gmail.com>
2018-07-04 17:53:01 +0900

    연산자
    누적대입 연산자까지 정리

--- day01-Hello/src/type/casting/TypeCastingTest.java
+++ day01-Hello/src/type/casting/TypeCastingTest.java
@@ -12,7 +12,7 @@ public class TypeCastingTest {
 	public static void main(String[] args) {
 		// 1. 선언, 2. 초기화
 		double pi = 3.14159265357979323846;
-		int	number = 100;
+		int number = 100;
 		int result;
 		
 		// (타입)피연산값; <== 피 연산값을 강제 형변환 구문
